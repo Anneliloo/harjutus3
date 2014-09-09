3b5b47c66354fb3d7279bf76a3fc466e5964fa61
Anneliloo <anneli.loo94@gmail.com>
2014-09-09 15:24:38 +0300

    Add sample strings

--- index.php
+++ index.php
@@ -17,6 +17,29 @@
 		$variable = 20;
 		echo $variable;
 	?>
+	
+	<?php 
+		// Selles näites on "Hello world!" string, mis koosneb 12 tähemärgist (ka tühik ning hüüumärk on tähemärgid).
+		echo "Hello world!"; ?> 
+		
+<h1>Stringid</h1>
+	<?php
+		echo "Hello world!";
+		echo "<br>"; 
+		echo 'Hello again!';
+		echo "<br>"; 
+	?>
+	<?php 
+		$variable = 24;
+		echo $variable;
+		?>
+	<?php 
+		$variable =26;
+		echo $variable;
+		?>
+	<?php 
+		echo "2426"; ?>
+	
 
 </body>
 
