1bd00f2990bb83024a41baca63dcd6c5f5b16a60
Anneliloo <anneli.loo94@gmail.com>
2014-09-09 15:10:22 +0300

    Add sample variables

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-tere
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html";charset=utf-8">
+	<title> Muutujad </title>
+</head>
+
+<body>
+	<?php
+		$variable = 10;
+		echo $variable;
+		// Lisan veebilehitsejas parema loetavuse saamiseks reamurdmise elemendi:
+		echo "<br>";
+	?>
+	
+	<?php
+		$variable = 20;
+		echo $variable;
+	?>
+
+</body>
+
+</html>
