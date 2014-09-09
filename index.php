cd2aa53875b5fa3c14df04ff313c31325e295dbe
Anneliloo <anneli.loo94@gmail.com>
2014-09-09 16:39:31 +0300

    add numbers

--- index.php
+++ index.php
@@ -94,7 +94,52 @@
     // Suvaline number vahemikus 7 - 22:
     echo rand(7, 22);
 	?>
+<br>
+	
+	<?php
+	$sample_nr = 45;
+	?>
+	<?php
+	$sample_nr = 45;
+	echo $sample_nr;
+	$sample_nr = $sample_nr + 5;
+	echo $sample_nr;
+	$sample_nr = $sample_nr + 5;
+	echo $sample_nr;
+	?>
+	<?php
+    $sample_nr2 = 20;
+    $sample_nr2 += 5;
+	$sample_nr2 += 5;
+    echo $sample_nr2;
+	?>
+	<?php
+    $sample_nr2 = 45;
+    $sample_nr2 -= 5;
+    echo $sample_nr2;
+?>
+<?php
+    $sample_nr3 = 45;
+    $sample_nr3 *= 5;
+    echo $sample_nr3;
 
+    $sample_nr2 = 45;
+    $sample_nr2 /= 5;
+    echo $sample_nr2;
+?>
+<br>
+<?php
+    echo 5 . " teksapüksid";
+?>
+<?php
+    echo 5 + " teksapüksid";
+?>
+<?php
+    echo 5 + "5";
+?>
+<?php
+    echo 5 + "7 teksapüksi";
+?>
 </body>
 
 </html>
