99676eb94fce988ef14d996c9b8722b65b9b5061
Anneliloo <anneli.loo94@gmail.com>
2014-09-09 16:03:51 +0300

    add interpolation example

--- index.php
+++ index.php
@@ -56,6 +56,9 @@
 		echo"<li>" . $student4 . "</li>";
 		echo"<li>" . $student5 . "</li>";
 		echo"<li>" . $fullName . "</li>";
+		
+		$test = "Testmime";
+		echo "$test kas interpolatsioon töötab";
 	?>
 		
 	
